91265269665b8a671da4a861b4a1496f55d07b79
anf692 <an.fall5@isepat.edu.sn>
2025-05-21 10:39:04 +0100

    Ajout de la permission

--- activité/views.py
+++ activité/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+class TaskViewSets(viewsets.ModelViewSet):
+    queryset= Task.objects.all()
+    serializer_class=TaskSerializer
+    permission_classes=[IsAuthenticated]
+
+
+
+
+
